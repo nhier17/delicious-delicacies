7c12ff5b48add83165379729427d010ee59146d7
Abraham <abrahamnhier97@gmail.com>
2024-01-02 11:22:35 +0300

    completed cusine page

--- src/Pages/Cuisine.jsx
+++ src/Pages/Cuisine.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from "styled-components"
+import {motion} from "framer-motion"
+import { Link, useParams } from 'react-router-dom'
+import { api_key } from '../api'
+
 
 function Cuisine() {
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+    useEffect(()=>{
+        getCuisine(params.type);
+    }, [params.type])
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${api_key}&cuisine=${name}`)
+        const recipes = await data.json();
+        setCuisine(recipes.results)
+    }
   return (
-    <div>Cuisine</div>
+    <Grid>
+        {cuisine.map(item => {
+            return (
+                <Card key={item.id}>
+                <img src={item.image} alt={item.title}/>
+                <h4>{item.title}</h4>
+                </Card>
+            )
+        })}
+    </Grid>
   )
 }
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit,minmax(20rem,1fr));
+    grid-gap: 3rem;
+`
+const Card = styled.div`
+a {
+    text-decoration: none;
+}
+
+img {
+    border-radius: 2rem;
+    width: 100%;
+    
+}
+h4 {
+    padding: 1rem;
+    text-align: center;
+  
+}
+`
 
 export default Cuisine
